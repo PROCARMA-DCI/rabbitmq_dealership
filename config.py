de1aaca76d01f5145749b9a1fd99713033c7ccc5
sbshamail123 <sbshamail123@gmail.com>
2025-08-25 11:08:16 +0500

    env added

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
-SECRET_KEY = "kazmi-irfan-ghalib-786"
-# RABBITMQ_HOST = "localhost"
-RABBITMQ_HOST = "52.34.69.230"
-RABBITMQ_PORT = 5672
-RABBITMQ_USERNAME = "dealership_user"
-RABBITMQ_PASSWORD = "secure_password_123"
-RABBITMQ_VHOST = "dealership_vhost"
-DATABASE_URL = "mysql+pymysql://mypcp_aws:gB5cT2nU@new-aws-mypcp.ctpynugu6t4m.us-west-2.rds.amazonaws.com/mypcp_livepcp?charset=utf8mb4"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "52.34.69.230")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+RABBITMQ_USERNAME = os.getenv("RABBITMQ_USERNAME")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
+RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SENDGRID_SMTP = os.getenv("SENDGRID_SMTP")
+SENDGRID_USER = os.getenv("SENDGRID_USER")
+SENDGRID_PASS = os.getenv("SENDGRID_PASS")
+BASE_URL = os.getenv("BASE_URL")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
